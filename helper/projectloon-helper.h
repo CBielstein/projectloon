174739326973879d2697b600ab4391c9db757f83
Cameron Bielstein <CTBielstein@gmail.com>
2014-11-18 16:41:16 -0600

    Basics to make it run.

---
+++ helper/projectloon-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef PROJECTLOON_HELPER_H
+#define PROJECTLOON_HELPER_H
+
+#include "ns3/projectloon.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* PROJECTLOON_HELPER_H */
+
---
+++ model/projectloon.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef PROJECTLOON_H
+#define PROJECTLOON_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* PROJECTLOON_H */
+
