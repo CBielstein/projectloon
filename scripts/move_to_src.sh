76fd08e5153205752ca770fbe8bf2c2f42a9865f
Cameron Bielstein <CTBielstein@gmail.com>
2014-11-18 17:06:12 -0600

    I think this should work

--- scripts/move_to_src.sh
+++ scripts/move_to_src.sh
@@ -15,9 +15,19 @@ cp /ns3/bake/source/ns-3.21/wscript ./
 
 # Put ProjectLoon repo in to src
 cd src
-git clone https://github.com/CBielstein/ProjectLoon.git
-
-cd -
+git clone https://github.com/CBielstein/projectloon.git
 cd -
 
-echo "Go change your goloon alias in ~/.profile to point to ...bake/ns-3.21/src/ProjectLoon instead of ...bake/ns-3.21/ProjectLoon"
+# Update waf configuration
+./waf --build-profile=debug --enable-examples --enable-tests configure
+
+# Update waf build
+./waf
+
+# Run test hello-loon
+./waf --run hello-loon
+
+cd -
+
+echo "Your goloon alias is now out of date."
+echo "Edit your profile (vim ~/.profile) and change alias goloon='cd \$NS3DIR/ProjectLoon' to alias goloon='cd \$NS3DIR/src/projectloon'"
